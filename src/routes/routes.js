fbf5aa580bcf006c4f69b9118c3f5f6a3b341f22
yuanmingze <yuanmingze@yaoyi.net>
2022-12-13 16:04:43 +0800

    删除注释

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,6 @@
-// import React from 'react'
 import Login from '@/pages/login/index.jsx'
 import Layout from '@/pages/layout/index.jsx'
 import Home from '@/pages/home/index.jsx'
-// const Home = React.lazy(() => import('@/pages/home/index.jsx'))
 
 const routes = [
   {
